345a266bf3cb40d0b4d97434ec6603f3ee59aeb9
Yashveer1234 <141895202+Yashveer1234@users.noreply.github.com>
2026-02-10 22:19:43 -0800

    Add Movie class with title and duration attributes

---
+++ Movie.java
@@ -0,0 +1,17 @@
+public class Movie {
+    private String title;
+    private double duration;
+
+    public Movie(String t, double d) {
+        title = t;
+        duration = d;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+}
